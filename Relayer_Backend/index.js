853b9ae1641b146ad99e6b20ce3f92f57962b52c
muskbuster <sudeepkamat79@gmail.com>
2023-08-26 17:40:02 +0000

    Something works

--- Relayer_Backend/index.js
+++ Relayer_Backend/index.js
@@ -13,7 +13,7 @@ app.post('/invoke', async (req, res) => {
     const tx = await invokeContract(sender, DataHash, inputData1,signature1);
 
     // Serialize the transaction to hex and send it in the response
-    res.status(200).json({ message: 'Transaction signed and serialized', transaction: tx.serialize(true) });
+    res.status(200).json({ message: 'Transaction signed and serialized'});
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'An error occurred' });
--- Relayer_Backend/transactionUtils.js
+++ Relayer_Backend/transactionUtils.js
@@ -1,12 +1,13 @@
 const AWS = require('aws-sdk');
 const neonjs = require('@cityofzion/neon-js');
 const { default: Neon, tx, rpc, u } = require("@cityofzion/neon-js");
-const rpcClient = new rpc.RPCClient("http://localhost:30333");
+const rpcClient = new rpc.RPCClient("http://seed2t5.neo.org:20332");
 AWS.config.update({
   accessKeyId: 'AKIA3KF6B62DPYNFR3DW',
   secretAccessKey:'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',
   region: 'us-east-1'
 });
+const vars = {};
 
 const kms = new AWS.KMS();
 
@@ -26,30 +27,41 @@ async function signWithKMS(message) {
  async function invokeContract( sender,Datahash,inputData1,signature1) {
 
   const script = neonjs.sc.createScript({
-    scriptHash: '0xfbf8174549453abb09ab7192a2e7083d9977efce',
-    operation: 'ExecuteFunction',
+    scriptHash: '0x7203d97ab14fd45b547c30b3f25548130c336100',
+    operation: 'executeFunction',
     args: [
-      neonjs.default.create.contractParam("PublicKey", sender),
-      neonjs.default.create.contractParam("String", Datahash),
-      neonjs.default.create.contractParam("String", "signature1"),
-      neonjs.default.create.contractParam("Integer", inputData1)
+      neonjs.default.create.contractParam("PublicKey", "03e20a907a0b5d0e4bbf1af9b0e609803bc141998d9a11d7d770f75d7e2fa1bb56"),
+      neonjs.default.create.contractParam("ByteArray", "010101"), // Corrected hexadecimal string
+      neonjs.default.create.contractParam("ByteArray", "010101"),
+      neonjs.default.create.contractParam("Integer", 10),
     ]
   });
   console.log(script);
   console.log("------------------------------------------");
+  const currentHeight = await rpcClient.getBlockCount();
+  console.log("\u001b[32m  ✓ Current block height: \u001b[0m", currentHeight);
+vars.tx = new tx.Transaction ({
+  signers: [
+    {
+    account: '',
+    scopes: tx.WitnessScope.CalledByEntry,
+  },
+  ],
+  script: script,
+  validUntilBlock: currentHeight+ 10000,
+});
+console.log("\u001b[32m  ✓ Transaction created \u001b[0m");
+console.log(vars.tx.serialize(true));
+serialsig=vars.tx.serialize(true);
   //sign the transaction with kms
-  const signature = await signWithKMS(script);
+  const signature = await signWithKMS(serialsig);
+  console.log("------------------------------------------");
+  console.log("------------------------------------------");
   console.log(signature);
   //create the transaction
-  // Construct the invocation transaction
-const invocationTx = neonjs.tx.InvocationTransaction.createInvocationTx([], [], script);
-invocationTx.addWitness(neonjs.tx.Witness.fromSignature(signature, 'ECC_NIST_P256'));
-console.log(invocationTx);
-  // Send the transaction to the network
-  const response = await rpcClient.sendRawTransaction(invocationTx.serialize(true));
-  console.log(response);
-  return response;
-
+  // Create a transaction object
+const result = await rpcClient.sendRawTransaction(signature);
+console.log(result);
 
 }
 
